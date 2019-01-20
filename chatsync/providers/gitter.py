edf837b0a596080d39b908e94853fb72f59110da
Chase Sterling <chase.sterling@gmail.com>
2019-01-20 16:54:13 -0500

    Fix gitter setup for sending messages

--- chatsync/providers/gitter.py
+++ chatsync/providers/gitter.py
@@ -17,16 +17,13 @@ class Provider(object):
             "Accept": "application/json",
             "Authorization": "Bearer {}".format(self.config["token"])
         }
+        self.connected = False
+        self.bot_id = None
+        self.room_id = None
 
-    async def send_message(self, client, message):
-        msg = "<{user} ({source})> {text}".format(**message)
-        try:
-            r = await client.post('https://api.gitter.im/v1/rooms/{}/chatMessages'.format(self.config["room"]),
-                                  json={"text": msg}, headers=self.headers, raise_for_status=True)
-        except aiohttp.ClientError as e:
-            log.exception('Error sending message.')
-
-    async def get_messages(self, client):
+    async def connect(self, client):
+        if self.connected:
+            return
         try:
             r = await client.get('https://api.gitter.im/v1/user/me', headers=self.headers, raise_for_status=True)
             user_info = await r.json()
@@ -34,7 +31,7 @@ class Provider(object):
             log.exception("Error connecting to gitter.")
             raise
         log.info("Connected to gitter with user %s", user_info["username"])
-        bot_id = user_info["id"]
+        self.bot_id = user_info["id"]
         try:
             r = await client.post("https://api.gitter.im/v1/rooms", json={"uri": self.config["room"]},
                                  raise_for_status=True, headers=self.headers)
@@ -43,7 +40,20 @@ class Provider(object):
             log.exception("Error joining gitter room %s", self.config["room"])
             raise
         log.info("Joined gitter room %s", room_info["name"])
-        room_id = room_info["id"]
+        self.room_id = room_info["id"]
+        self.connected = True
+
+    async def send_message(self, client, message):
+        await self.connect(client)
+        msg = "<{user} ({source})> {text}".format(**message)
+        try:
+            r = await client.post('https://api.gitter.im/v1/rooms/{}/chatMessages'.format(self.room_id),
+                                  json={"text": msg}, headers=self.headers, raise_for_status=True)
+        except aiohttp.ClientError as e:
+            log.exception('Error sending message.')
+
+    async def get_messages(self, client):
+        await self.connect(client)
         while True:
             try:
                 r = await client.get('https://stream.gitter.im/v1/rooms/{}/chatMessages'.format(room_id),
