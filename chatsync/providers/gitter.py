dbca9b249c2440f25320e39279572f51d4c23462
Chase Sterling <chase.sterling@gmail.com>
2019-01-20 16:46:02 -0500

    Make gitter room config use friendly name Add more logging/error handling to gitter

--- chatsync/providers/gitter.py
+++ chatsync/providers/gitter.py
@@ -27,23 +27,38 @@ class Provider(object):
             log.exception('Error sending message.')
 
     async def get_messages(self, client):
-        r = await client.get('https://api.gitter.im/v1/user/me', headers=self.headers)
-        user_info = await r.json()
+        try:
+            r = await client.get('https://api.gitter.im/v1/user/me', headers=self.headers, raise_for_status=True)
+            user_info = await r.json()
+        except aiohttp.ClientError as e:
+            log.exception("Error connecting to gitter.")
+            raise
+        log.info("Connected to gitter with user %s", user_info["username"])
         bot_id = user_info["id"]
         try:
-            r = await client.get('https://stream.gitter.im/v1/rooms/{}/chatMessages'.format(self.config["room"]),
-                                 headers=self.headers, timeout=None, raise_for_status=True)
-            while True:
-                data = await r.content.readline()
-                if not data.strip():
-                    continue
-                gitter_msg = json.loads(data)
-                if gitter_msg["fromUser"]["id"] == bot_id:
-                    continue
-                yield {
-                    "user": gitter_msg["fromUser"]["username"],
-                    "user_avatar": gitter_msg["fromUser"]["avatarUrl"],
-                    "text": gitter_msg["text"]
-                }
+            r = await client.post("https://api.gitter.im/v1/rooms", json={"uri": self.config["room"]},
+                                 raise_for_status=True, headers=self.headers)
+            room_info = await r.json()
         except aiohttp.ClientError as e:
-            log.exception('Error getting messages. Restarting connection.')
+            log.exception("Error joining gitter room %s", self.config["room"])
+            raise
+        log.info("Joined gitter room %s", room_info["name"])
+        room_id = room_info["id"]
+        while True:
+            try:
+                r = await client.get('https://stream.gitter.im/v1/rooms/{}/chatMessages'.format(room_id),
+                                     headers=self.headers, timeout=None, raise_for_status=True)
+                while True:
+                    data = await r.content.readline()
+                    if not data.strip():
+                        continue
+                    gitter_msg = json.loads(data)
+                    if gitter_msg["fromUser"]["id"] == bot_id:
+                        continue
+                    yield {
+                        "user": gitter_msg["fromUser"]["username"],
+                        "user_avatar": gitter_msg["fromUser"]["avatarUrl"],
+                        "text": gitter_msg["text"]
+                    }
+            except aiohttp.ClientError as e:
+                log.exception('Error getting messages. Restarting connection.')
